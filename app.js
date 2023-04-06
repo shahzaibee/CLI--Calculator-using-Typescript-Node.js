5b36d98f120b452a0a46d3c77b01a61552c89dab
Shahzaib786 <125633854+shahzaibee@users.noreply.github.com>
2023-04-06 22:35:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const myPromise = new Promise((resolve, rejected) => {
+    console.log('Promise pending');
+    setTimeout(() => {
+        //console.log('Promise resolve');
+        //resolve(["Shahzaib", "Honey"])
+        console.log('promise rejected');
+        rejected(new Error('Data fetch reject'));
+    }, 2000);
+});
+myPromise.then((res) => (console.log(res))).catch((err) => (console.log('new error')));
+export {};
+//Apply conditional statement
+/*
+const myPromise = new Promise((resolve, rejected) => {
+  console.log("Promise pending");
+
+  setTimeout(() => {
+    let data = "";
+
+    if (data) {
+      console.log("promise resolved");
+      return resolve(data);
+    } else {
+      console.log("promise rejected");
+      return rejected("New Error");
+    }
+  }, 2000);
+});
+
+myPromise
+  .then((res) => console.log(res))
+  .catch((err) => console.log("new error"));
+  */
---
+++ index.ts
@@ -0,0 +1,101 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+
+const sleep = () => {
+  return new Promise((res) => {
+    setTimeout(res, 2000);
+  });
+};
+
+async function welcome() {
+  let rainbowTitle = chalkAnimation.rainbow("Lets start calculation");
+  await sleep();
+  rainbowTitle.stop();
+  console.log();
+  console.log(
+    chalk.blue(`_____________________
+    |  _________________  |
+    | | JO           0. | |
+    | |_________________| |
+    |  ___ ___ ___   ___  |
+    | | 7 | 8 | 9 | | + | |
+    | |___|___|___| |___| |
+    | | 4 | 5 | 6 | | - | |
+    | |___|___|___| |___| |
+    | | 1 | 2 | 3 | | x | |
+    | |___|___|___| |___| |
+    | | . | 0 | = | | / | |
+    | |___|___|___| |___| |
+    |_____________________|)`)
+  );
+}
+await welcome();
+
+async function askQuestion() {
+ await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "operator",
+        message: "Which operation you want to perform",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+      },
+      {
+        type: "number",
+        name: "num1",
+        message: "Enter your first number:",
+      },
+      {
+        type: "number",
+        name: "num2",
+        message: "Enter your second number:",
+      },
+    ])
+    .then((answers) => {
+      if (answers.operator == "Addition") {
+        console.log(
+          `${answers.num1} + ${answers.num2}= ${answers.num1 + answers.num2} `
+        );
+      }
+      else if (answers.operator == "Subtraction") {
+        console.log(
+          `${answers.num1} - ${answers.num2}= ${answers.num1 - answers.num2} `
+        );
+      }
+      else if (answers.operator == "Multiplication") {
+        console.log(
+          `${answers.num1} * ${answers.num2}= ${answers.num1 * answers.num2} `
+        );
+      }
+      else if (answers.operator == "Division") {
+        console.log(
+          `${answers.num1} / ${answers.num2}= ${answers.num1 / answers.num2} `
+        );
+      }
+    });
+}
+//askQuestion();
+
+async function startAgain(){
+
+do{
+await askQuestion();
+var again = await inquirer.prompt([
+
+    {
+        type:"input",
+        name:"restart",
+        message:"Do you want to contine press y or n:",
+    },
+]);
+}while (
+    again.restart == "y" ||
+    again.restart == "Y" ||
+    again.restart == "yes" ||
+    again.restart == "YES"
+  );
+}
+startAgain();
